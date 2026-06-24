ed32f9bd929411041f04e60e9e60fb028b9eec23
reallynniroprobably <aidenpeteryoung@gmail.com>
2026-06-25 09:40:37 +1000

    Updated testing main

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,11 @@ mod lex;
 fn main() {
     let test: &str = "test";
     let input: String = fs::read_to_string(format!("tests/{test}.cb"))
-        .unwrap_or_default();
-    let output: String = lex::lex_file(&input).iter().map(|a| a.v.as_str()).collect::<Vec<_>>().join("\n");
-    fs::write("tests/test_output.txt", output)
-        .expect("Failed to write output");
+        .expect("Failed to read input from file");
+    let output: lex::LexedFile = lex::lex_program(&input);
+    let s_output = output.0;
+    let t_output = output.1;
+    fs::write(format!("tests/{test}_output.txt"), s_output.join("\n"))
+        .expect("Failed to write output to file");
+    
 }
