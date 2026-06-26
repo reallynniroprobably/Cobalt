00f6c66a753a2594a6ee281f65886dae6679ada2
reallynniroprobably <aidenpeteryoung@gmail.com>
2026-06-26 10:31:59 +1000

    Update lexer file name

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::fs;
-mod lex;
+mod lexer;
 
 fn main() {
     let test: &str = "test";
     let input: String = fs::read_to_string(format!("tests/{test}.cb"))
         .expect("Failed to read input from file");
-    let output: lex::LexedFile = lex::lex_program(&input);
+    let output: lexer::LexedFile = lexer::lex_program(&input);
     let s_output = output.0;
     let t_output = output.1;
     fs::write(format!("tests/{test}_output.txt"), s_output.join("\n"))
